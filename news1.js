23016075ab3d2a549cef377e82f839a851afb66f
nathaliamar <natha.com@gmail.com>
2022-07-18 11:37:49 -0500

    updating news1 2 and 3

--- news1.js
+++ news1.js
@@ -3,7 +3,7 @@ newsSummary1 = "Robotics Team were winners at the Minnesota Technology Fair this
 newsImage1 = "/Common/resources/DesignPortfolio/SiteThemes/CommonLib/news/robot.jpg";
 NewsHtml1 = "The school Robotics Team were winners at the Minnesota Technology Fair this past weekend where their robot, entirely crafted from carbon fiber. See pictures of the robot – which was name “Bozo” by the Grade 10 Science classes that worked together to build it – right here!";
 
-$('.add-new-page-modal-form-title').children('input').val(newsTitle1);
+$('.add-new-page-modal-form-title').children('input').val(newsTitle1).trigger("change");
 
 $('.tbl_row_text:contains("Title")').next().children("input").val(newsTitle1);
 $('label:contains("Summary")').parent("td").next().children("textarea").val(newsSummary1);
@@ -12,11 +12,6 @@ $('.tbl_row_text:contains("Featured Image")').each(function(){
 	$(this).next().children("input").first().val(newsImage1);
 });
 
-
 $('label:contains("Summary")').parent("td").next().children("textarea").val(newsSummary1);
-
-//$('textarea.reTextArea').show(0).val(NewsHtml1);
-
-$('.reContentCell').children("iframe").hide(0)
-$('.reContentCell').append('<textarea class="reTextArea" style="height: 173px;"></textarea>');
+$('table .reMode_html').parent().trigger("click");
 $('.reContentCell textarea.reTextArea').val(NewsHtml1);
--- news3.js
+++ news3.js
@@ -1,7 +1,7 @@
-newsTitle1 = "Welcome Dr. Waltz";
-newsSummary1 = "We are pleased to welcome Dr. Waltz";
-newsImage1 = "/Common/resources/DesignPortfolio/SiteThemes/CommonLib/news/new-program.jpg";
-NewsHtml1 = "We are pleased to welcome Dr. Waltz as the new Head of the Faculty of Sciences. Dr. Waltz is a former young member of the Space X project and is sure to bring a world of experience to our Faculty of Sciences.";
+newsTitle1 = "School Financial Plan Available Now";
+newsSummary1 = "Financial Plan has been released for this year.";
+newsImage1 = "/Common/resources/DesignPortfolio/SiteThemes/CommonLib/news/financial-plan.jpg";
+NewsHtml1 = "We have released our Financial Plan for this year. Members of the County are free to review this public document and attend a public meeting with a date TBD.";
 
 $('.add-new-page-modal-form-title').children('input').val(newsTitle1).trigger("change");
 
@@ -15,5 +15,3 @@ $('.tbl_row_text:contains("Featured Image")').each(function(){
 $('label:contains("Summary")').parent("td").next().children("textarea").val(newsSummary1);
 $('table .reMode_html').parent().trigger("click");
 $('.reContentCell textarea.reTextArea').val(NewsHtml1);
-
-
