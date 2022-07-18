1f11255e08786a85842f30b3b1c1ac6cdbcf102a
nathaliamar <natha.com@gmail.com>
2022-07-18 11:04:41 -0500

    updating news2.js

--- news2.js
+++ news2.js
@@ -1,9 +1,9 @@
-newsTitle1 = "Welcome Dr. Waltz!";
+newsTitle1 = "Welcome Dr. Waltz";
 newsSummary1 = "We are pleased to welcome Dr. Waltz";
 newsImage1 = "/Common/resources/DesignPortfolio/SiteThemes/CommonLib/news/new-program.jpg";
 NewsHtml1 = "We are pleased to welcome Dr. Waltz as the new Head of the Faculty of Sciences. Dr. Waltz is a former young member of the Space X project and is sure to bring a world of experience to our Faculty of Sciences.";
 
-$('.add-new-page-modal-form-title').children('input').val(newsTitle1);
+$('.add-new-page-modal-form-title').children('input').val(newsTitle1).trigger("change");
 
 $('.tbl_row_text:contains("Title")').next().children("input").val(newsTitle1);
 $('label:contains("Summary")').parent("td").next().children("textarea").val(newsSummary1);
@@ -20,3 +20,5 @@ $('label:contains("Summary")').parent("td").next().children("textarea").val(news
 $('.reContentCell').children("iframe").hide(0)
 $('.reContentCell').append('<textarea class="reTextArea" style="height: 173px;"></textarea>');
 $('.reContentCell textarea.reTextArea').val(NewsHtml1);
+
+
