3f304b19652403f205b8fb189d9e2011b4f06525
nathaliamar <35434251+nathaliamar@users.noreply.github.com>
2022-08-30 22:14:59 -0500

    Update devbar.js

--- devbar.js
+++ devbar.js
@@ -3,5 +3,5 @@ jQuery(document).ready(function(){
 	var url = window.location.href;
 	$("body").empty();
 	$("body").prepend('<iframe width="100%" height="100%" frameborder="0" src="'+ url +'"></iframe>');
-	$("body").prepend("<div class="developerBar"><a href="/calendar">calendar</a></div>");
+	$("body").prepend('<div class="developerBar"><a href="/calendar">calendar</a></div>');
 })
