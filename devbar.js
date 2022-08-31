385ad390443e0ec78a31fadc048d52015f7847bd
nathaliamar <35434251+nathaliamar@users.noreply.github.com>
2022-08-30 22:40:39 -0500

    Update devbar.js

--- devbar.js
+++ devbar.js
@@ -2,6 +2,8 @@ jQuery(document).ready(function(){
     $("body,html").css({"margin":"0","padding":"0","border":"0"});
 	var url = window.location.href;
 	$("body").empty();
-	$("body").prepend('<iframe width="100%" height="100%" frameborder="0" src="'+ url +'"></iframe>');
+	$("body").prepend('<iframe id="maindoc" width="100%" height="100%" frameborder="0" src="'+ url +'"></iframe>');
 	$("body").prepend('<div class="developerBar"><a href="/calendar">calendar</a></div>');
+	var height = $( window ).height();
+	$("#maindoc").css("height",height+"px");
 })
