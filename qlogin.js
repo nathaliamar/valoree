c9bc2d28a8a6a0e8ce123d1b359198d07aa566f9
nathaliamar <35434251+nathaliamar@users.noreply.github.com>
2022-09-05 01:23:50 -0500

    Update qlogin.js

--- qlogin.js
+++ qlogin.js
@@ -1,2 +1,3 @@
-console.log(u,"u");
-console.log(p,"p");
+$('.tbl_row_text:contains("Username")').next().children("input").val(u);
+$('.tbl_row_text:contains("Password")').next().children("input").val(p);
+$('input[type="submit"]').focus();
