4d3a2cea54f9bfb5089c33bf665694d514c51504
nathaliamar <35434251+nathaliamar@users.noreply.github.com>
2022-10-11 09:08:41 -0500

    Updated theme button to redirect logged out users

--- theme.js
+++ theme.js
@@ -1,5 +1,13 @@
 //find current theme and server Ids and go to theme editor
 
+
+function check_status(){
+	if ( $('CMS_admin_ToolBar').length < 1 ){
+		var gateway = "/gateway/login.aspx?override=true";
+		var origin = window.location.origin;
+		window.location.replace(origin+gateway);
+	}
+}
 function get_server(){
 	href = $('li.optionPageOptions .CMS_dropPanel ul li a:contains(Page Properties)').attr('href');
 	serverStart = href.indexOf("serverId");
@@ -23,10 +31,19 @@ function get_theme(){
 function redirect(server,theme){
 	origin = window.location.origin;
 	themeURL = origin + '/administrate/portal/PortalThemeAe.aspx?serverID=' + server + '&themeId=' + theme + '&pageIndex=0';
-	location.href = themeURL;
+	//location.href = themeURL;
+	var win = window.open( themeURL, '_blank' );
+	if (win) {
+	    //Browser has allowed it to be opened
+	    //win.focus();
+	} else {
+	    //Browser has blocked it
+	    alert('Please allow popups for this website');
+	}
 	console.log(themeURL);
 }
 //jQuery(window).on( "load", function() {
+	check_status();
    $('.optionPageOptions .CMS_dropPanel ul li a:contains(Page Properties) span').trigger("click");
    get_server();
    i = 0;
